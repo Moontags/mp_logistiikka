58442e2c088acbf51f76830242ca8b80da825ab9
Jari Peltola <jena9988@gmail.com>
2026-05-25 19:36:33 +0300

    Picture

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [390, 768, 1024, 1280, 1920],
